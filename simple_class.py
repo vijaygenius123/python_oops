9b6ba5de3149aa89bcb7fc2b416e797f39a10491
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-06-23 23:44:16 +0530

    Class with initializer function __init__

--- simple_class.py
+++ simple_class.py
@@ -1,9 +1,14 @@
 
 # Create A Basic Class
 class Book():
-    pass
+    
+    def __init__(self, title):
+        self.title = title
 
 
 # Create an instance of the class
-b1 = Book()
+b1 = Book("Brave New World")
+b2 = Book("War & Peace")
 
+print(b1)
+print(b1.title)
