e459fba1462a4c878ac21df66b2ebb86a2c92b29
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-06-23 23:48:40 +0530

    Example of method in class

--- simple_class.py
+++ simple_class.py
@@ -2,13 +2,19 @@
 # Create A Basic Class
 class Book():
     
-    def __init__(self, title):
+    def __init__(self, title, author, pages, price):
         self.title = title
+        self.author = author
+        self.pages = pages
+        self.price = price
+    
+    def get_price(self):
+        return self.price
 
 
 # Create an instance of the class
-b1 = Book("Brave New World")
-b2 = Book("War & Peace")
+b1 = Book("Brave New World","Leo Tolstoy", 1225, 39.95)
+b2 = Book("War & Peace","JD Salinger",234,12.95)
 
-print(b1)
+print(b1.get_price())
 print(b1.title)
